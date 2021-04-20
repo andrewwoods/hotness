b17b8f0b766f377664c244f1adcca1da1255b908
Andrew Woods <services@awoods.net>
2021-04-20 12:12:57 -0400

    Update sample.php with more PHP features
    This helps demonstrate what your PHP code will look like.

--- colors/sample.php
+++ colors/sample.php
@@ -1,10 +1,10 @@
 #!/usr/bin/php
 <?php
 
-require_once "vendor/autoload.php";
 
 namespace Hotness;
 
+require_once './hotness.vim';
 
 # Shell style comment
 
@@ -36,12 +36,17 @@ class Math {
     // Pi to 20 places
     const PI = 3.14159265358979323846;
 
-    protected $author = "Andrew Woods";
+    /**
+     * @var string
+     */
+    protected string $author = "Andrew Woods";
 
-    protected $date_published = '2021-04-15';  
+    /**
+     * @var string
+     */
+    protected string $date_published = '2021-04-15';
 
     protected $is_published = true;  
-
 
     public function __construct() {
         // Intentionally empty
@@ -56,6 +61,17 @@ class Math {
     }
 }
 
+$data = [
+    [
+        'name' => 'Joe Biden',
+        'job' => 'President',
+    ],
+    [
+        'name' => 'Kamala Harris',
+        'job' => 'Vice President',
+    ]
+];
+
 foreach ($data as $item) {
     echo 'item name=' . $item->name; 
 }
@@ -65,9 +81,11 @@ for ($i=0, $max = count($data) - 1; $i <= $max; $i++ ) {
     echo 'item name=' . $data[$i]->name; 
 }
 
+
 try {
     $math = new Math();
 } catch (Exception $e) {
     error_log($e->getMessage());
 }
 
+echo Math::PI;
